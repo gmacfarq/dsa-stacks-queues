fca5c79908d07c9684f1a158b527f8462ecc92af
Jakob <shavinski.jakob@gmail.com>
2023-06-12 15:43:37 -0700

    finish stacks

--- stack.js
+++ stack.js
@@ -19,26 +19,41 @@ class Stack {
   /** push(val): add new value to the top of the stack. Returns undefined. */
 
   push(val) {
+    let node = new Node(val);
 
+    if (this.top !== null) {
+      node.next = this.top;
+    } 
+
+    this.top = node;
+    this.size++;
+    return undefined;
   }
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
   pop() {
+    if (this.isEmpty()) throw new Error('empty');
 
+    let oldTop = this.top;
+    let nextNode = this.top.next;
+    this.top = nextNode;
+    this.size--;
+    
+    return oldTop.val;
   }
 
   /** peek(): return the value of the top node in the stack. */
 
   peek() {
-
+    return this.top.val;
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
-
+    return this.size === 0;
   }
 }
 
